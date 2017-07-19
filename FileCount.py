8c51935cfd28b644ed175845034438c8deb65b11
Jyoti Amage <jyoti_amage@persistent.co.in>
2017-07-19 15:21:11 +0530

    Script for counting files in directory

---
+++ FileCount.py
@@ -0,0 +1,18 @@
+import os,os.path
+
+def file_Count_in_Dir():
+    print(len([name for name in os.listdir('d:') if os.path.isfile(name)]))
+
+def file_list():
+    ls=os.listdir('.')
+    file_count = len(ls)
+    print(file_count)
+
+def file_only():
+    onlyfiles=next(os.walk(r'D:\TEM-Tivoli Endpoint Manager\Git_Repo\site-Patches_for_Ubuntu_1404\Fixlets\amd64\Security'))[2]
+    print(len(onlyfiles))
+
+file_only()
+#file_list()
+#file_Count_in_Dir()
+
